d858e11a6f11b9a702dc9afd963dbf477111da51
ViniHGV <ixxvinicius@gmail.com>
2024-06-24 15:40:53 -0300

    feat: iniciando o projeto

---
+++ src/main/java/com/picpay_simplified/PicpaySimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.picpay_simplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaySimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaySimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay_simplified/PicpaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay_simplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
